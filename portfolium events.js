6c4df72c57a0d34347bcb3a83cbe86c59b968a85
Tiago Sousa <tiagosousa028@gmail.com>
2016-03-08 04:08:30 +0000

    adapt code to add/edit features

--- portfolium events.js
+++ portfolium events.js
@@ -1,8 +1,6 @@
-previous = "";
-margin = limit = position = line = 0;
+prev_event = time = "";
+margin = limit = position = line = eventPos = 0;
 side = "left";
-switchside = true;
-time = "future";
 today = new Date();
 global_events = [
   {
@@ -22,46 +20,51 @@ $(document).ready(function(){
   function addEvent(events){
     $.each(events, function(index){
       time = (this.begin_date > today ? "future" : "past");
+      var cur_event = this;
 
-      if(this.type == "milestone"){
+      $.each(global_events, function(index){
+        if(this.begin_date <= cur_event.begin_date)
+          eventPos = index;
+      });
+
+      if(cur_event.type == "milestone"){
         if(side=="left"){
-          var eltemp = $('#meetus_box_left_milestone').clone();
-          $(eltemp).css("display", "").appendTo(".meetus.left");
+          var eltemp = $('#events_box_left_milestone').clone();
+          $(eltemp).css("display", "").appendTo(".events.left");
         }
         else {
-          var eltemp = $('#meetus_box_right_milestone').clone();
-          $(eltemp).css("display", "").appendTo(".meetus.right");
+          var eltemp = $('#events_box_right_milestone').clone();
+          $(eltemp).css("display", "").appendTo(".events.right");
         }
         $(eltemp).css("margin-top", ((time ? "future" : "past") != time ? limit+70 : margin) + "px");
-        previous = "milestone";
+        prev_event = global_events[index].type;
       }
       else{
         if(side=="left"){
-          var eltemp = $('#meetus_box_left').clone();
-          $(eltemp).css("display", "").appendTo(".meetus.left");
+          var eltemp = $('#events_box_left').clone();
+          $(eltemp).css("display", "").appendTo(".events.left");
         }
         else{
-          var eltemp = $('#meetus_box_right').clone();
-          $(eltemp).css("display", "").appendTo(".meetus.right");
+          var eltemp = $('#events_box_right').clone();
+          $(eltemp).css("display", "").appendTo(".events.right");
         }
         $(eltemp).css("margin-top", ((time ? "future" : "past") != time ? limit+70 : margin) + "px");
-        previous = "show";
+        prev_event = global_events[index].type;
       }
 
-      $(eltemp).find(".event_title").text(this.title);
-      $(eltemp).find(".event_date").text(this.date);
-      $(eltemp).find(".event_description").text(this.description);
-      if(this.type == "show")
-        if(!this.image)
+      $(eltemp).find(".event_title").text(cur_event.title);
+      $(eltemp).find(".event_date").text(cur_event.date);
+      $(eltemp).find(".event_description").text(cur_event.description);
+      if(cur_event.type == "show")
+        if(!cur_event.image)
           $(eltemp).find('img').last().css("display", "none");
         else
-          $(eltemp).find('img').last().attr('src', this.image);
+          $(eltemp).find('img').last().attr('src', cur_event.image);
 
-      // position += $(eltemp).height() + (this.image ? ($(eltemp).find('img').last().height() < 40 ? this.imageHeight.height : 0) : 0) + parseInt($(eltemp).css("margin-top"));
+      // position += $(eltemp).height() + (cur_event.image ? ($(eltemp).find('img').last().height() < 40 ? cur_event.imageHeight.height : 0) : 0) + parseInt($(eltemp).css("margin-top"));
       position += $(eltemp).height() + parseInt($(eltemp).css("margin-top"));
 
       if(position >= limit || limit - position < 20){
-        switchSide = true;
         side = (side == "left" ? "right" : "left");
         margin = (limit ? (position - limit) : 80);
         if(margin > 100)
@@ -69,9 +72,9 @@ $(document).ready(function(){
         else if(margin < 20)
           margin = 50;
         limit = position - limit;
-        if((time ? "future" : "past") != time){
-          $(".meetus.center .event_line_future").css("height", line+130);
-          $(".meetus.center hr").css("display","");
+        if(time == "future"){
+          $(".events.center .event_line_future").css("height", line+130);
+          $(".events.center hr").css("display","");
           line = 0;
           time = "past"; 
           margin +=50;
@@ -80,32 +83,30 @@ $(document).ready(function(){
         position = 0;
       }
       else{
-        switchSide = false;
         margin = 50;
       }
 
       if(time){
-        $(".meetus.center .event_line_future").css("height",limit + 120);
-        $(".meetus.center hr").css("display","");
+        $(".events.center .event_line_future").css("height",limit + 120);
+        $(".events.center hr").css("display","");
       }
     });
 
-    $(".meetus.center .event_line_past").css("height", Math.max($(".meetus.right").height(), $(".meetus.left").height()) + 80);
+    $(".events.center .event_line_past").css("height", Math.max($(".events.right").height(), $(".events.left").height()) + 80);
   };
-
-  addEvent(global_events);
 
   $(".menu form").on("submit", function(event) {
     // Prevents the form from being submited
     event.preventDefault();
+    var errors = [];
 
-    this.begin_date.value;
-    this.end_date.value;
-    this.type.value = "Milestone";
+    if(new Date(this.begin_date.value) > new Date(this.end_date.value)){
+      errors.push("Begin date can't be higher than end date.");
+    }
 
     event = [
       {
-        type: this.type.value,
+        type: "Milestone",
         future: true,
         title: this.title.value,
         begin_date: new Date(this.begin_date.value),
@@ -118,5 +119,6 @@ $(document).ready(function(){
     addEvent(event);
   });
 
+  addEvent(global_events);
 });
 
